7609950bb0f82f5e78b40701e8db26463447de3f
Sergey Orlov <skillzorwispa@gmail.com>
2019-07-24 15:19:34 +0700

    index-mobile.html --ok

--- public/src/js/scripts.js
+++ public/src/js/scripts.js
@@ -1,27 +1,79 @@
-var registerButtonShow = document.querySelector(".register-button");
-var registerButtonHide = document.querySelector(".register-popup-close");
-var registerPopup = document.querySelector("#register");
-var login = document.querySelector("#login");
 
-var mapButtonShow = document.querySelector("#js-map-show");
-var mapButtonHide = document.querySelector(".map-popup-close");
-var mapPopup = document.querySelector("#map");
+//:::::: SLIDER EDGES ::::::::
 
-registerButtonShow.addEventListener("click", function(evt){
-	evt.preventDefault();
-    registerPopup.classList.add("register-popup-show");
-	login.focus();
-});
-registerButtonHide.addEventListener("click", function(evt){
-    evt.preventDefault();
-    registerPopup.classList.remove("register-popup-show");
-});
-mapButtonShow.addEventListener("click", function(evt){
-	evt.preventDefault();
-	mapPopup.classList.add("map-popup-show");
-});
-mapButtonHide.addEventListener("click", function(evt){
-	evt.preventDefault();
-	mapPopup.classList.remove("map-popup-show");
-});
-/*map-popup-show*/
+{
+
+	var edges_button1 = document.querySelector("#edges__bullit-1");
+	var edges_button2 = document.querySelector("#edges__bullit-2");
+	var edges_button3 = document.querySelector("#edges__bullit-3");
+
+	var edges_slide1 = document.querySelector("#edges__slide-1");
+	var edges_slide2 = document.querySelector("#edges__slide-2");
+	var edges_slide3 = document.querySelector("#edges__slide-3");
+
+	edges_button1.addEventListener("click", function(evt){
+		evt.preventDefault();
+		removeClass();
+		edges_slide1.classList.add("active");
+	});
+	edges_button2.addEventListener("click", function(evt){
+		evt.preventDefault();
+		removeClass();
+		edges_slide2.classList.add("active");
+	});
+	edges_button3.addEventListener("click", function(evt){
+		evt.preventDefault();
+		removeClass();
+		edges_slide3.classList.add("active");
+	});
+
+	function removeClass(){
+		if(edges_slide1.classList.contains("active")){
+			edges_slide1.classList.remove("active");
+		}
+		if(edges_slide2.classList.contains("active")){
+			edges_slide2.classList.remove("active");
+		}
+		if(edges_slide3.classList.contains("active")){
+			edges_slide3.classList.remove("active");
+		}
+	}
+}
+
+{
+	var button1 = document.querySelector("#about__bullit-1");
+	var button2 = document.querySelector("#about__bullit-2");
+	var button3 = document.querySelector("#about__bullit-3");
+
+	var slide1 = document.querySelector("#about__slide-1");
+	var slide2 = document.querySelector("#about__slide-2");
+	var slide3 = document.querySelector("#about__slide-3");
+
+	button1.addEventListener("click", function(evt){
+		evt.preventDefault();
+		removeClass();
+		slide1.classList.add("active");
+	});
+	button2.addEventListener("click", function(evt){
+		evt.preventDefault();
+		removeClass();
+		slide2.classList.add("active");
+	});
+	button3.addEventListener("click", function(evt){
+		evt.preventDefault();
+		removeClass();
+		slide3.classList.add("active");
+	});
+
+	function removeClass(){
+		if(slide1.classList.contains("active")){
+			slide1.classList.remove("active");
+		}
+		if(slide2.classList.contains("active")){
+			slide2.classList.remove("active");
+		}
+		if(slide3.classList.contains("active")){
+			slide3.classList.remove("active");
+		}
+	}
+}
